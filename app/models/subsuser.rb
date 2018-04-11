2fd4a1b3570f6b7e0c0c400f830cba7a60a5fe4c
viktorgg <vikor.g.g@abv.bg>
2018-04-11 15:47:34 +0300

    commit new changes

--- app/models/subsuser.rb
+++ app/models/subsuser.rb
@@ -1,4 +1,37 @@
 class Subsuser < ApplicationRecord
 	belongs_to :user
 	belongs_to :sub
+	validate :validation
+end
+
+def validation
+	if self.sub < 10
+		if self.sub.subuser.where(role: "teacher").count >= 2 
+			&& self.role == "teacher" self.error.add(:user, "No more than 2 teachers" 
+			throw :abort
+		end
+		
+		if self.sub.sunuser.where(role: "student").count >= 10 
+			&& self.role == "student" self.error.add(:user, "No more than 10 users"
+			throw :abort
+		end
+	end
+	if sub.price < 10
+    	 sunuser.all.each do |item|
+    	 	if record.subscription_id == connection.subscription_id && record.role == "Student"    	 		
+    	 		students+=1
+    	 	else 
+    	 		teachers+=1
+    	 	end
+    	 end
+    	 if connection.role == "Student"
+    	 	students +=1;
+    	 else 
+    	 	teachers+=1;
+    	 end
+    	 if students > 10 || teachers > 2 
+    	 	 connection.errors[:base] << "You cant do that "
+    	 end   
+    end
+  end
 end
