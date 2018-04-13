429e32b1a7bbe3553484eb9918a81840d9163dd2
viktorgg <viktor.g.g@abv.bg>
2018-04-13 20:11:17 +0300

    Update subsuser.rb

--- app/models/subsuser.rb
+++ app/models/subsuser.rb
@@ -15,23 +15,5 @@ def validation
 			&& self.role == "student" self.error.add(:user, "No more than 10 users"
 			throw :abort
 		end
-	end
-	if sub.price < 10
-    	 sunuser.all.each do |item|
-    	 	if record.subscription_id == connection.subscription_id && record.role == "Student"    	 		
-    	 		students+=1
-    	 	else 
-    	 		teachers+=1
-    	 	end
-    	 end
-    	 if connection.role == "Student"
-    	 	students +=1;
-    	 else 
-    	 	teachers+=1;
-    	 end
-    	 if students > 10 || teachers > 2 
-    	 	 connection.errors[:base] << "You cant do that "
-    	 end   
-    end
-  end
+  	end
 end
